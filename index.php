92373731bb2cec439a87873c877f3333a9331981
jbosegre <jbose@mail.greenriver.edu>
2019-01-09 14:16:08 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: humme_000
+ * Date: 1/9/2019
+ * Time: 11:18 AM
+ */?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>You do too</h1>
+</body>
+</html>
